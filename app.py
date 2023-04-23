eea8dcae9ca98d66e278b756db51e13be1d11d2f
Kevin Rocha Stettner <38251063+Dexstettner@users.noreply.github.com>
2023-04-23 17:48:00 -0300

    Bug Added

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "labdevops-grupo31"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
+
 if __name__ == '__main__':
     app.run()

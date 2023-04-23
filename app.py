76dedd778fb67bb5761bd1c6fc6fb016e51b697f
Kevin Rocha Stettner <38251063+Dexstettner@users.noreply.github.com>
2023-04-23 15:54:05 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "FIAP-09ASO-GRUPO3-BOTA-FE"
+    return "labdevops-9aso-grupo31"
 
 if __name__ == '__main__':
     app.run()
